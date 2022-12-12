47a1225898e2343b112f1271f2e7fb0d1459c3fe
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-12-12 18:01:05 +0100

    Bid api get and add endpoints created, refactored endpoints mapping.

---
+++ Api/WebAppBidExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniValidation;
+
+namespace Api.Data;
+
+public static class WebAppBidExtensions
+{
+    public static void MapBidEndpoints(this WebApplication app)
+    {
+        app.MapGet("house/{houseId:int}/bids", async (int houseId, IHouseRepository houseRepo, IBidRepository bidRepo) => 
+        {
+            if (await houseRepo.Get(houseId) == null)
+            {
+                return Results.Problem($"House with id {houseId} not found.");
+            }
+
+            var bids = await bidRepo.Get(houseId);
+            return Results.Ok(bids);
+
+        }).ProducesProblem(404).Produces(statusCode: 200);
+
+        app.MapPost("house/{houseId:int}/bids",
+            async (int houseId, [FromBody] BidDto dto, IBidRepository repository) => 
+            {
+                if (dto.HouseId != houseId)
+                { 
+                    return Results.Problem("No match", statusCode: StatusCodes.Status400BadRequest);
+                }
+
+                if (!MiniValidator.TryValidate(dto, out var errors))
+                { 
+                    return Results.ValidationProblem(errors);
+                }
+
+                var newBid = await repository.Add(dto);
+
+                return Results.Created($"/houses/{newBid.HouseId}/bids", newBid);
+            }).ProducesProblem(statusCode: 400).Produces<BidDto>(statusCode: 201).ProducesValidationProblem();
+    }
+}
---
+++ Api/WebAppHouseExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniValidation;
+
+namespace Api.Data;
+
+public static class WebAppHouseExtensions
+{
+    public static void MapHouseEndpoints(this WebApplication app)
+    {
+        app.MapGet("/houses", 
+                (IHouseRepository repository) => repository.GetAll())
+            .Produces<HouseDto>(statusCode: 200);
+        
+        app.MapGet("/house/{houseId:int}", 
+            async (int houseId, IHouseRepository repository) =>
+        {
+            var house = await repository.Get(houseId);
+        
+            if (house == null)
+            {
+                return Results.Problem($"House with ID {houseId} not found.", statusCode: 404);
+            }
+        
+            return Results.Ok(house);
+        }).ProducesProblem(404).Produces<HouseDetailDto>(statusCode: 200);
+        
+        app.MapPost("/houses", 
+            async ([FromBody] HouseDetailDto dto, IHouseRepository repository) =>
+        {
+            if (!MiniValidator.TryValidate(dto, out var errors))
+            {
+                return Results.ValidationProblem(errors);
+            }
+        
+            var newHouse = await repository.Add(dto);
+        
+            return Results.Created($"/house/{newHouse.HouseId}", newHouse);
+        }).Produces<HouseDetailDto>(StatusCodes.Status201Created).ProducesValidationProblem();
+        
+        app.MapPut("/houses", 
+            async ([FromBody] HouseDetailDto dto, IHouseRepository repository) =>
+        {
+            if (!MiniValidator.TryValidate(dto, out var errors))
+            {
+                return Results.ValidationProblem(errors);
+            }
+            
+            if (await repository.Get(dto.HouseId) == null)
+            {
+                return Results.Problem($"House {dto.HouseId} not found.", statusCode: 404);
+            }
+        
+            var updatedHouse = await repository.Update(dto);
+        
+            return Results.Ok(updatedHouse);
+        }).ProducesProblem(404).Produces<HouseDetailDto>(statusCode: 200).ProducesValidationProblem();
+        
+        app.MapDelete("/houses/{houseId:int}",
+            async (int houseId, IHouseRepository repository) =>
+        {
+            if (await repository.Get(houseId) == null)
+            {
+                return Results.Problem($"House {houseId} not found.", statusCode: 404);
+            }
+        
+            await repository.Delete(houseId);
+        
+            return Results.Ok();
+        }).ProducesProblem(404).Produces(statusCode: 200);
+    }
+}
